3c6d3310e159098935e5876f3c06e18171c67c02
Jerry Ablan <jerryablan@dreamfactory.com>
2015-03-24 14:43:13 -0400

    Composer dependencies updated.

--- resources/views/layouts/partials/head-master.blade.php
+++ resources/views/layouts/partials/head-master.blade.php
@@ -2,10 +2,10 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>@yield('page-title', 'Welcome!') | DreamFactory Enterprise&trade;</title>
-<link href="/static/bootswatch-3.3.4/{{ config('dfe.common.auth.theme', 'darkly') }}.min.css" rel="stylesheet">
+<link href="/static/bootswatch-3.3.4/{{ config('dfe.common.theme', 'darkly') }}.min.css" rel="stylesheet">
 <link href="/static/font-awesome-4.3.0/css/font-awesome.min.css" rel="stylesheet">
 <link href="/css/style.css" rel="stylesheet">
-<link href="/vendor/dfe-common/css/auth.{{ config('dfe.common.auth.theme', 'darkly') }}.css" rel="stylesheet">
+<link href="/vendor/dfe-common/css/auth.{{ config('dfe.common.theme', 'darkly') }}.css" rel="stylesheet">
 <!--[if lt IE 9]>
 <script src="//oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
 <script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
