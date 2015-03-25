08a8a9ea6a88a5fc8671b16188803510b11a10b0
Jerry Ablan <jerryablan@dreamfactory.com>
2015-03-25 10:49:15 -0400

    Current work checkpoint. See diff for details.

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -10,7 +10,6 @@
 	@show
 	<link href="/static/font-awesome-4.3.0/css/font-awesome.min.css" rel="stylesheet">
 	<link href="/css/style.css" rel="stylesheet">
-	<link href="/vendor/dfe-common/css/auth.css" rel="stylesheet">
 	<!--[if lt IE 9]>
 	<script src="//oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
 	<script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->
