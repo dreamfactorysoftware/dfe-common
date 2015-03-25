b4f322832f3b102de8de31fdb2f1a3efd86cd34e
Jerry Ablan <jerryablan@dreamfactory.com>
2015-03-25 10:30:02 -0400

    Removed theme name from css file

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('page-title', 'Welcome!') | DreamFactory Enterprise&trade;</title>
-    @section('head-theme')
-        <link href="/vendor/dfe-common/static/bootswatch-3.3.4/darkly.min.css" rel="stylesheet">
-    @show
-    <link href="/static/font-awesome-4.3.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="/css/style.css" rel="stylesheet">
-    <link href="/vendor/dfe-common/css/auth.{{ config('dfe.common.themes.auth', 'darkly') }}.css" rel="stylesheet">
-    <!--[if lt IE 9]>
-    <script src="//oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
-    <script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>@yield('page-title', 'Welcome!') | DreamFactory Enterprise&trade;</title>
+	@section('head-theme')
+		<link href="/vendor/dfe-common/static/bootswatch-3.3.4/darkly.min.css" rel="stylesheet">
+	@show
+	<link href="/static/font-awesome-4.3.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href="/css/style.css" rel="stylesheet">
+	<link href="/vendor/dfe-common/css/auth.css" rel="stylesheet">
+	<!--[if lt IE 9]>
+	<script src="//oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
+	<script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->
 
-    @section('head-links')
-    @show
+	@section('head-links')
+	@show
 
-    @section('head-scripts')
-    @show
+	@section('head-scripts')
+	@show
 </head>
 <body class="@yield('body-class')">
 
 <div id="page-content" class="container-fluid">
-    @yield('content')
+	@yield('content')
 </div>
 
 @section('before-body-scripts')
