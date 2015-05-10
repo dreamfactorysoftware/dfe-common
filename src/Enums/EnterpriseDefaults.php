66a293f0c615a6be55e2bc94e9de8c9099643497
Jerry Ablan <jerryablan@dreamfactory.com>
2015-05-09 21:39:30 -0400

    Current work checkpoint. See diff for details.

--- src/Enums/EnterpriseDefaults.php
+++ src/Enums/EnterpriseDefaults.php
@@ -19,55 +19,15 @@ class EnterpriseDefaults extends FactoryEnum
     /**
      * @var string
      */
-    const DFE_ENDPOINT = 'http://cerberus.fabric.dreamfactory.com/api';
+    const DFE_ENDPOINT = 'http://console.enterprise.dreamfactory.com/api/v1/ops';
     /**
      * @var string
      */
-    const DFE_AUTH_ENDPOINT = 'http://cerberus.fabric.dreamfactory.com/api/instance/credentials';
+    const DFE_AUTH_ENDPOINT = 'http://console.enterprise.dreamfactory.com/api/v1/ops/credentials';
     /**
      * @var string
      */
-    const OASYS_PROVIDER_ENDPOINT = 'http://oasys.cloud.dreamfactory.com/oauth/providerCredentials';
-    /**
-     * @var string
-     */
-    const INSTANCE_CONFIG_FILE_NAME_PATTERN = '/instance.json';
-    /**
-     * @var string
-     */
-    const DB_CONFIG_FILE_NAME_PATTERN = '/{instance_name}.database.config.php';
-    /**
-     * @var string
-     */
-    const PLATFORM_VIRTUAL_SUBDOMAIN = '.cloud.dreamfactory.com';
-    /**
-     * @type string
-     */
-    const INSTALL_ROOT_MARKER = '/.dreamfactory.php';
-    /**
-     * @type string
-     */
-    const COMPOSER_MARKER = '/vendor/autoload.php';
-    /**
-     * @var string
-     */
-    const FABRIC_MARKER = '/var/www/.fabric_hosted';
-    /**
-     * @var string
-     */
-    const ENTERPRISE_MARKER = '/var/www/.dfe_hosted';
-    /**
-     * @var string
-     */
-    const DEFAULT_DOC_ROOT = '/var/www/launchpad/web';
-    /**
-     * @var string
-     */
-    const DEFAULT_DEV_DOC_ROOT = '/opt/dreamfactory/dsp/dsp-core/web';
-    /**
-     * @var string
-     */
-    const MAINTENANCE_MARKER = '/var/www/.dfe_maintenance';
+    const MAINTENANCE_MARKER = '/var/www/.maintenance';
     /**
      * @var string
      */
@@ -83,11 +43,11 @@ class EnterpriseDefaults extends FactoryEnum
     /**
      * @var string Public storage cookie key
      */
-    const PUBLIC_STORAGE_COOKIE = 'dfe.storage_id';
+    const PUBLIC_STORAGE_COOKIE = 'dfe-console.public-id';
     /**
      * @var string Private storage cookie key
      */
-    const PRIVATE_STORAGE_COOKIE = 'dfe.private_storage_id';
+    const PRIVATE_STORAGE_COOKIE = 'dfe-console.private-id';
     /**
      * @type string
      */
