0a6088611258300618cf0840291fbd5df3179b36
Jerry Ablan <jerryablan@dreamfactory.com>
2015-05-29 14:03:32 -0400

    Current work checkpoint. See diff for details.

--- config/dfe.common.php
+++ config/dfe.common.php
@@ -5,15 +5,14 @@
 
 return [
     /** Global options */
-    'display-name'      => 'DreamFactory Enterprise',
-    'display-version'   => 'v1.0.x-alpha',
+    'display-name'    => 'DreamFactory Enterprise',
+    'display-version' => 'v1.0.x-alpha',
     /**
      * Theme selection -- a bootswatch theme name
      * Included are cerulean, darkly, flatly, paper, and superhero.
      * You may also install other compatible themes and use them as well.
      */
-    'themes'            => ['auth' => 'darkly', 'page' => 'flatly'],
-    'log-path'          => env( 'DFE_LOG_PATH' ),
-    'log-file-name'     => env( 'DFE_LOG_FILE_NAME' ),
-    'registration-open' => false,
+    'themes'          => ['auth' => 'darkly', 'page' => 'flatly'],
+    'log-path'        => env( 'DFE_LOG_PATH' ),
+    'log-file-name'   => env( 'DFE_LOG_FILE_NAME' ),
 ];
