ee6b5fe6c9ec49131221acb7f1a91abbea6bb1a5
Jerry Ablan <jerryablan@dreamfactory.com>
2016-02-12 18:39:25 -0500

    Notification work

--- src/Traits/Notifier.php
+++ src/Traits/Notifier.php
@@ -1,5 +1,6 @@
 <?php namespace DreamFactory\Enterprise\Common\Traits;
 
+use DreamFactory\Enterprise\Console\Enums\ConsoleOperations;
 use DreamFactory\Enterprise\Database\Models\Instance;
 use DreamFactory\Enterprise\Database\Models\ServiceUser;
 use DreamFactory\Library\Utility\Json;
@@ -47,13 +48,15 @@ trait Notifier
      */
     protected function notify($email, $name, $subject, array $data)
     {
-        try {
-            empty($this->subjectPrefix) && $this->subjectPrefix = config('provisioning.email-subject-prefix');
-            $subject = $this->subjectPrefix . ' ' . trim(str_replace($this->subjectPrefix, null, $subject));
-            $data['dashboard_url'] = config('dfe.dashboard-url');
-            $data['support_email_address'] = config('dfe.support-email-address');
+        $_view = array_get($data, 'email-view', 'emails.generic');
 
-            $_result = \Mail::send('emails.generic',
+        try {
+            $this->subjectPrefix = $this->subjectPrefix ?: config('provisioning.email-subject-prefix');
+            $subject = $this->subjectPrefix . ' ' . trim(str_replace($this->subjectPrefix, null, $subject));
+
+            $data = array_merge($this->getNotificationDefaultData(), $data);
+
+            $_result = \Mail::send($_view,
                 $data,
                 function($message/** @var Message $message */) use ($email, $name, $subject) {
                     $message->from(config('mail.from.address'), config('mail.from.name'));
@@ -76,9 +79,87 @@ trait Notifier
 
             @file_put_contents(date('YmdHis') . '-' . $email . '.json',
                 Json::encode(array_merge($data,
-                    ['subject' => $subject, 'template' => 'emails.generic', 'email' => $email, 'name' => $name,])));
+                    ['subject' => $subject, 'email-view' => $_view, 'email' => $email, 'name' => $name,])));
 
             return false;
         }
+    }
+
+    /**
+     * @param string $operation The job type
+     * @param string $email     The email of the recipient
+     * @param string $name      The recipient's name
+     * @param array  $data      Any view data
+     * @param string $view      The email view to use
+     *
+     * @see \DreamFactory\Enterprise\Console\Enums\ConsoleOperations
+     * @return int
+     */
+    protected function notifyJobOwner($operation, $email, $name, array $data = [], $view = null)
+    {
+        /** @type Instance $_instance */
+        if (null !== ($_instance = array_get($data, 'instance'))) {
+            $data['instanceUrl'] = $_instance->getProvisionedEndpoint();
+            $data['instanceName'] = $_instance->instance_name_text;
+        }
+
+        if ($_instance && null === ($_firstName = array_get($data, 'firstName'))) {
+            $_firstName = $_instance->user->first_name_text;
+            $data['firstName'] = $_firstName;
+        }
+
+        $_headTitle = $_contentHeader = null;
+
+        switch (trim(strtolower($operation))) {
+            case ConsoleOperations::METRICS:
+                $_headTitle = 'Metrics Complete';
+                $_contentHeader = 'Metrics have been generated successfully';
+                $view = $view ?: 'emails.generic';
+                break;
+
+            case ConsoleOperations::PROVISION:
+                $_headTitle = 'Provisioning Complete';
+                $_contentHeader = 'Your new instance is ready';
+                $view = $view ?: 'emails.provision';
+                break;
+
+            case ConsoleOperations::DEPROVISION:
+                $_headTitle = 'Deprovisioning Complete';
+                $_contentHeader = 'Your instance has been retired';
+                $view = $view ?: 'emails.deprovision';
+                break;
+
+            case ConsoleOperations::IMPORT:
+                $_headTitle = 'Import Complete';
+                $_contentHeader = 'Your imported instance is ready';
+                $view = $view ?: 'emails.generic';
+                break;
+
+            case ConsoleOperations::EXPORT:
+                $_headTitle = 'Export Complete';
+                $_contentHeader = 'Your export is complete';
+                $view = $view ?: 'emails.generic';
+                break;
+
+            default:
+                throw new \InvalidArgumentException('The operation "' . $operation . '" is invalid.');
+        }
+
+        ($_headTitle && !isset($data['headTitle'])) && $data['headTitle'] = $_headTitle;
+        ($_contentHeader && !isset($data['contentHeader'])) && $data['contentHeader'] = $_contentHeader;
+        $data['email-view'] = $view;
+
+        return $this->notify($email, $name, $_headTitle, $data);
+    }
+
+    /**
+     * @return array The view data that is always available
+     */
+    protected function getNotificationDefaultData()
+    {
+        return [
+            'dashboard_url'         => config('dfe.dashboard-url'),
+            'support_email_address' => config('dfe.support-email-address'),
+        ];
     }
 }
